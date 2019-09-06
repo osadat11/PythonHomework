54ae02c4b53f2a55aa631b2223ab5d89ee5e0268
datt16 <osatom.4639@gmail.com>
2019-09-07 01:30:59 +0900

    jsonへの対応(1)
    - "/"にアクセスしたときにjsonを返すように変更
    - テーブルの構成変更に伴いCRUD操作の関数をすべてコメントアウト
    
    テーブル(todo.db > posts)の構成を変更
    - colum: (time, tag)を追加
    - column: done にはdefault制約を追加
    
    テーブルの初期化用にinit.sqlを追加

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_cors import CORS
 
 DEBUG = True
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['JSON_AS_ASCII'] = False 
 
 db=SQLAlchemy(app)
 db.create_all()
@@ -17,62 +19,79 @@ class Post(db.Model):
     done = db.Column(db.Integer)
     title = db.Column(db.Text())
     date = db.Column(db.Text())
+    time = db.Column(db.Text())
     content = db.Column(db.Text())
-    
+    tag = db.Column(db.Text())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "done": self.done,
+            "title": self.title,
+            "date": self.date,
+            "time": self.time,
+            "content": self.content,
+            "tag": self.tag
+        }
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
-    return render_template('index.html', posts = posts)
+    posts = Post.query.get(1)
+    result = {
+        "tasks": [
+            Post.to_dict(post) for post in Post.query.all()
+        ]
+    }
+    return jsonify(result)
 
-@app.route("/new")
-def new_post():
-    return render_template("new.html")
+# @app.route("/new")
+# def new_post():
+#     return render_template("new.html")
 
-@app.route("/create", methods=["POST"])
-def create_post():
-    new_post = Post()
-    new_post.done = 0
-    new_post.title = request.form["title"]
-    new_post.content = request.form["content"]
-    new_post.date = str(datetime.today().year) + "-" + str(datetime.today().month) + "-" + str(datetime.today().day)
-    db.session.add(new_post)
-    db.session.commit()
+# @app.route("/create", methods=["POST"])
+# def create_post():
+#     new_post = Post()
+#     new_post.done = 0
+#     new_post.title = request.form["title"]
+#     new_post.content = request.form["content"]
+#     new_post.date = str(datetime.today().year) + "-" + str(datetime.today().month) + "-" + str(datetime.today().day)
+#     db.session.add(new_post)
+#     db.session.commit()
 
-    return redirect(url_for('.index'))
+#     return redirect(url_for('.index'))
 
-@app.route("/edit/<int:id>")
-def edit_post(id):
-    post=Post.query.get(id)
-    return render_template("edit.html", post=post)
+# @app.route("/edit/<int:id>")
+# def edit_post(id):
+#     post=Post.query.get(id)
+#     return render_template("edit.html", post=post)
 
-@app.route("/update/<int:id>", methods=["POST"])
-def update_post(id):
-    post = Post.query.get(id)
-    post.title = request.form["title"]
-    post.content = request.form["content"]
-    post.date = str(datetime.today().year) + "-" + str(datetime.today().month) + "-" + str(datetime.today().day)
-    db.session.commit()
-    return redirect(url_for('.index'))
+# @app.route("/update/<int:id>", methods=["POST"])
+# def update_post(id):
+#     post = Post.query.get(id)
+#     post.title = request.form["title"]
+#     post.content = request.form["content"]
+#     post.date = str(datetime.today().year) + "-" + str(datetime.today().month) + "-" + str(datetime.today().day)
+#     db.session.commit()
+#     return redirect(url_for('.index'))
 
-@app.route("/done/<int:id>", methods=["POST"])
-def done_post(id):
-    request_id = str("checkbox-" + str(id))
-    post = Post.query.get(id)
-    if request.form.get(request_id):
-        post.done = 1
-    else:
-        post.done = 0
-    db.session.commit()
-    return redirect(url_for('.index'))
+# @app.route("/done/<int:id>", methods=["POST"])
+# def done_post(id):
+#     request_id = str("checkbox-" + str(id))
+#     post = Post.query.get(id)
+#     if request.form.get(request_id):
+#         post.done = 1
+#     else:
+#         post.done = 0
+#     db.session.commit()
+#     return redirect(url_for('.index'))
 
-@app.route("/delete/<int:id>")
-def delete_post(id):
-    post=Post.query.get(id)
-    db.session.delete(post)
-    db.session.commit()
-    posts = Post.query.all()
-    return redirect(url_for('.index'))
+# @app.route("/delete/<int:id>")
+# def delete_post(id):
+#     post=Post.query.get(id)
+#     db.session.delete(post)
+#     db.session.commit()
+#     posts = Post.query.all()
+#     return redirect(url_for('.index'))
 
 
 if __name__ == '__main__':
---
+++ backend/init.sql
@@ -0,0 +1,31 @@
+-- DROP TABLE posts;
+
+-- CREATE TABLE posts (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     done INTEGER DEFAULT 0,
+--     title text,
+--     date text,
+--     time text,
+--     content text,
+--     tag text
+-- );
+
+-- INSERT INTO posts VALUES(
+--     0,
+--     0,
+--     'Test Tasks',
+--     '2019-09-07',
+--     '00-57',
+--     'これはテストです',
+--     'test-init-tag'
+-- );
+
+-- INSERT INTO posts(title,date,time,content,tag) VALUES(
+--     'PCKの練習',
+--     '2019-09-07',
+--     '01-12',
+--     '幅優先探索をできるように...',
+--     'test-競プロ'
+-- );
+
+SELECT * FROM posts
