ead6a8febc44aee557047918424d1d7ef8b46507
datt16 <osatom.4639@gmail.com>
2019-12-06 16:31:03 +0900

    リポジトリの整理

--- backend_n/test-query@insert.sql
+++
@@ -1,9 +0,0 @@
--- SQLite
-INSERT INTO tasks(title, description, date, done, tag) VALUES(
-    "PCKの練習",
-    "累積和、幅優先探索、深さ優先探索、全探索。全探索はしっかりかけるように",
-    "2019-09-12",
-    0,
-    'PCK,競プロ,?TEST'
-)
-SELECT * FROM tasks
